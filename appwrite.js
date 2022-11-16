3782a48eac5efb02554f81d0f695d7b1471a671c
opestro <mahdiharzallah21@gmail.com>
2022-11-16 20:02:10 +0100

    test

---
+++ appwrite.js
@@ -0,0 +1,12 @@
+import { Client, Account, Database } from "appwrite";
+
+const client = new Client();
+
+client
+    .setEndpoint('https://localhost/v1') // Your API Endpoint
+    .setProject('dash1') // Your project ID
+;
+const account = new Account(client);
+
+
+export {account};
